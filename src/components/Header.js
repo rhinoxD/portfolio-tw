dee7dbebb3dab97efe15c4639c3c5000acf7cab1
unknown <coolshiv0721@gmail.com>
2022-04-10 18:53:26 +0530

    Partial Navbar Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [showMenu, setShowMenu] = useState('md:hidden');
   const menuItems = [
     {
       title: 'Home',
@@ -19,10 +20,18 @@ function Header() {
   return (
     <div className='text-white font-mont'>
       <div className='flex bg-theme justify-between items-center p-2 shadow-lg'>
-        <div>
+        <div className='flex justify-between w-full'>
           <h1 className='text-4xl font-semibold'>S K</h1>
+          <button
+            onClick={() => {
+              setShowMenu();
+            }}
+            className='lg:hidden xl:hidden 2xl:hidden md:flex'
+          >
+            MENU
+          </button>
         </div>
-        <div className='flex'>
+        <div className='flex md:hidden'>
           {menuItems.map((item) => {
             return (
               <li className='list-none mx-5'>
