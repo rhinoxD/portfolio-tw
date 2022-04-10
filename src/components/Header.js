c79accefff54e48fe73b25c13eece8280598da73
unknown <coolshiv0721@gmail.com>
2022-04-10 19:51:04 +0530

    Made Navbar Responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 function Header() {
   const [showMenu, setShowMenu] = useState('md:hidden');
@@ -17,33 +18,50 @@ function Header() {
       key: '/contact',
     },
   ];
+  const pathname = window.location.pathname;
   return (
     <div className='text-white font-mont'>
-      <div className='flex bg-theme justify-between items-center p-2 shadow-lg'>
-        <div className='flex justify-between w-full'>
+      <div
+        className={`flex bg-theme justify-between items-center p-2 shadow-lg ${
+          showMenu == '' && 'md:flex-col'
+        }`}
+      >
+        <div className='flex justify-between items-center w-full'>
           <h1 className='text-4xl font-semibold'>S K</h1>
-          <button
+          <FaBars
             onClick={() => {
-              setShowMenu();
+              if (showMenu === 'md:hidden') {
+                setShowMenu('');
+              } else {
+                setShowMenu('md:hidden');
+              }
             }}
-            className='lg:hidden xl:hidden 2xl:hidden md:flex'
-          >
-            MENU
-          </button>
+            className='lg:hidden xl:hidden 2xl:hidden md:flex cursor-pointer'
+          />
         </div>
         <div className='flex md:hidden'>
           {menuItems.map((item) => {
             return (
-              <li className='list-none mx-5'>
+              <li
+                className={`list-none mx-5 p-1 ${
+                  item.key == pathname && 'bg-white text-black rounded-md'
+                }`}
+              >
                 <Link to={`${item.key}`}>{item.title}</Link>
               </li>
             );
           })}
         </div>
-        <div className='md-flex flex-col lg:hidden xl:hidden 2xl:hidden'>
+        <div
+          className={`md:flex mt-5 items-start w-full flex-col lg:hidden xl:hidden 2xl:hidden ${showMenu}`}
+        >
           {menuItems.map((item) => {
             return (
-              <li className='list-none mx-5'>
+              <li
+                className={`list-none py-1 ${
+                  item.key == pathname && 'bg-white text-black rounded-md'
+                }`}
+              >
                 <Link to={`${item.key}`}>{item.title}</Link>
               </li>
             );
