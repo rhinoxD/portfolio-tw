506fb840d7dbaab7ca9e9d676fb33def3707cecd
unknown <coolshiv0721@gmail.com>
2022-04-11 20:44:30 +0530

    Dev Stack

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,6 +41,7 @@ function Home() {
             </div>
           </div>
         </div>
+
         {/* Technologies */}
         <div className='mt-28'>
           <h1 className='text-4xl text-blue-800 font-bold text-center my-1'>
@@ -94,6 +95,7 @@ function Home() {
             /> */}
           </div>
         </div>
+
         {/* Javascript Section */}
         <div className='my-20'>
           <div className='text-center h-52 bg-primary'>
@@ -119,6 +121,55 @@ function Home() {
               because of its ubiquitous use on all platforms and mass adoption.
               Main Use Cases: Web Development.
             </p>
+          </div>
+        </div>
+
+        {/* Dev Stack Section */}
+        <div className='my-20'>
+          <div className='text-center h-52 bg-red-400'>
+            <h1 className='text-white font-bold text-3xl py-10'>
+              My DEV Stack
+            </h1>
+          </div>
+          <div
+            className='mx-56 md:mx-5 shadow-2xl bg-gray-50 -mt-20 
+          rounded-lg hover:bg-gray-700 hover:text-white'
+          >
+            <div className='h-96'>
+              <lottie-player
+                src='https://assets2.lottiefiles.com/packages/lf20_vybwn7df.json'
+                background='transparent'
+                speed='1'
+                loop
+                autoplay
+              ></lottie-player>
+            </div>
+            <div className='grid md:grid-cols-1 grid-cols-3 p-10 md:p-5'>
+              <div>
+                <h1 className='text-xl font-bold'>Front End</h1>
+                <hr />
+                <p className='font-semibold mt-2'>HTML/CSS</p>
+                <p className='font-semibold mt-2'>React</p>
+                <p className='font-semibold mt-2'>Javascript</p>
+                <p className='font-semibold mt-2'>Redux</p>
+              </div>
+              <div className='text-center md:mb-2'>
+                <h1 className='text-xl font-bold'>UI / UX</h1>
+                <hr />
+                <p className='font-semibold mt-2'>Bootstrap</p>
+                <p className='font-semibold mt-2'>Tailwind</p>
+                <p className='font-semibold mt-2'>Material UI</p>
+                <p className='font-semibold mt-2'>Semantic UI</p>
+              </div>
+              <div className='text-right'>
+                <h1 className='text-xl font-bold'>Backend and DB</h1>
+                <hr />
+                <p className='font-semibold mt-2'>Node JS</p>
+                <p className='font-semibold mt-2'>Express JS</p>
+                <p className='font-semibold mt-2'>My SQL</p>
+                <p className='font-semibold mt-2'>Mongo DB</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
