0ae4cc7135313ca12cb84b7d6c9fa1250fe90988
unknown <coolshiv0721@gmail.com>
2022-04-12 18:21:57 +0530

    Footer Steps

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,15 @@ import { FaGithub, FaLinkedin, FaMailBulk } from 'react-icons/fa';
 function Footer() {
   return (
     <>
+      <div className='h-44 md:h-20'>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+          <path
+            fill='#2a2a2a'
+            fill-opacity='1'
+            d='M0,128L0,128L288,128L288,256L576,256L576,64L864,64L864,160L1152,160L1152,64L1440,64L1440,320L1152,320L1152,320L864,320L864,320L576,320L576,320L288,320L288,320L0,320L0,320Z'
+          ></path>
+        </svg>
+      </div>
       <div className='bg-theme w-screen flex justify-center'>
         <div className='md:w-full w-1/2'>
           <div className='p-10 font-mont text-center'>
