eb34efb4994f47bc50443c8c30c18e7398c3cfc2
unknown <coolshiv0721@gmail.com>
2022-04-12 18:12:07 +0530

    Contact Page - Form Input and Styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -15,13 +15,26 @@ function Contact() {
           ></lottie-player>
         </div>
         <div className='w-screen flex justify-center'>
-          <div className='md:w-full w-1/2'>
+          <div className='md:w-full w-1/2 p-10 shadow-2xl bg-gray-50'>
             <h1 className='text-2xl font-semibold'>Contact me to know more</h1>
             <input
               type='text'
               placeholder='Name'
-              className='w-full border-2 border-gray-400'
+              className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
             />
+            <input
+              type='text'
+              placeholder='Email'
+              className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
+            />
+            <textarea
+              type='text'
+              placeholder='Query'
+              className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
+            />
+            <button className='bg-red-500 rounded text-white px-5 py-1 mt-3'>
+              SUBMIT
+            </button>
           </div>
         </div>
       </div>
