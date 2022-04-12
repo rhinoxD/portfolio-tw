7bf01c742c6f233413dbe54cc5cfb240a303b031
unknown <coolshiv0721@gmail.com>
2022-04-12 18:07:51 +0530

    Contact Page - Intro

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,27 @@ import Layout from '../components/Layout';
 function Contact() {
   return (
     <Layout>
-      <div>Contact</div>
+      <div>
+        <div className='h-screen mt-10'>
+          <lottie-player
+            src='https://assets2.lottiefiles.com/packages/lf20_kdx6cani.json'
+            background='transparent'
+            speed='1'
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+        <div className='w-screen flex justify-center'>
+          <div className='md:w-full w-1/2'>
+            <h1 className='text-2xl font-semibold'>Contact me to know more</h1>
+            <input
+              type='text'
+              placeholder='Name'
+              className='w-full border-2 border-gray-400'
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
