2860e58d9587ead73da6fc357858cda4723c2243
unknown <coolshiv0721@gmail.com>
2022-04-12 10:00:40 +0530

    Projects - Project Data Added

---
+++ src/resources/projects.js
@@ -0,0 +1,48 @@
+const projectsData = [
+  {
+    title: 'GoCamp',
+    image:
+      'https://us.123rf.com/450wm/nataliia2910/nataliia29101809/nataliia2910180900062/110071175-vector-illustration-of-camping-in-night-time-with-beautiful-view-on-mountains-family-camping-evening.jpg?ver=6',
+    description:
+      'GoCamp is a website where users can create and review campgrounds',
+    link: 'https://gocamp-rhino.herokuapp.com',
+  },
+  {
+    title: 'Colors-App',
+    image:
+      'https://addons.mozilla.org/user-media/previews/full/223/223386.png?modified=1625032655',
+    description:
+      'A color picker application which is a clone of Material-UI colors & Flat-UI colors made using React JS, Material Ui',
+    link: 'https://colors-app-rhino.herokuapp.com',
+  },
+  {
+    title: 'Insta-Clone',
+    image:
+      'https://i0.wp.com/apkmody.io/wp-content/uploads/2018/07/Instagram-Cover-Photo.jpg?resize=720%2C360&ssl=1',
+    description: 'Instagram Clone',
+    link: 'https://insta-rhino.herokuapp.com/signin',
+  },
+  {
+    title: 'DevConnector',
+    image:
+      'https://socialengineindia.com/blog/wp-content/uploads/2021/10/What-are-the-Core-features-of-a-Successful-Online-Community-Platform.png',
+    description: 'Social network for developers',
+    link: 'https://devconnector2-rhino.herokuapp.com',
+  },
+  {
+    title: 'ShoppingCart',
+    image:
+      'https://media.wired.com/photos/5c9040ee4950d24718d6da99/191:100/w_2400,h_1256,c_limit/shoppingcart-1066110386.jpg',
+    description: "Whatever you've got in mind, we've got inside.",
+    link: 'https://shoppingcart-rhino.herokuapp.com',
+  },
+  {
+    title: 'Rexx-Clothing',
+    image:
+      'https://cdn.pixabay.com/photo/2015/08/25/11/50/shop-906722_960_720.jpg',
+    description: 'A store that sells not just outfits but a trend.',
+    link: 'https://rexx-rhino.herokuapp.com',
+  },
+];
+
+export default projectsData;
