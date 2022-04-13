1ad32dc2a11e6e3bb0b3eb8fba9096c6a78e0073
unknown <coolshiv0721@gmail.com>
2022-04-13 09:55:15 +0530

    Contact Page Updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Layout from '../components/Layout';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [query, setQuery] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: 'post',
+      url: 'api/contact',
+      data: { name, email, query },
+    }).then((response) => {
+      if (response.data.status === 'success') {
+        alert('Message Sent.');
+        resetForm();
+      } else if (response.data.status === 'fail') {
+        alert('Message failed to send.');
+      }
+    });
+  };
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setQuery('');
+  };
   return (
     <Layout>
       <div>
@@ -16,25 +40,34 @@ function Contact() {
         </div>
         <div className='w-screen flex justify-center'>
           <div className='md:w-full w-1/2 p-10 shadow-2xl bg-gray-50'>
-            <h1 className='text-2xl font-semibold'>Contact me to know more</h1>
-            <input
-              type='text'
-              placeholder='Name'
-              className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
-            />
-            <input
-              type='text'
-              placeholder='Email'
-              className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
-            />
-            <textarea
-              type='text'
-              placeholder='Query'
-              className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
-            />
-            <button className='bg-red-500 rounded text-white px-5 py-1 mt-3'>
-              SUBMIT
-            </button>
+            <form onSubmit={handleSubmit} method='post'>
+              <h1 className='text-2xl font-semibold'>
+                Contact me to know more
+              </h1>
+              <input
+                type='text'
+                placeholder='Name'
+                className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type='email'
+                placeholder='Email'
+                className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <textarea
+                placeholder='Query'
+                className='w-full border-2 border-gray-400 rounded p-1 shadow-lg mt-5'
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <button className='bg-red-500 rounded text-white px-5 py-1 mt-3'>
+                SUBMIT
+              </button>
+            </form>
           </div>
         </div>
       </div>
